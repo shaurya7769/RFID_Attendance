88116786a4984c5b170e9baf71cd7735c71eceea
shaurya <52601483+shaurya7769@users.noreply.github.com>
2024-07-10 02:39:05 +0530

    Add files via upload

---
+++ RFID_Excel.ino
@@ -0,0 +1,89 @@
+#include <SPI.h>
+#include <MFRC522.h>
+
+#define SS_PIN 10 //RX slave select
+#define RST_PIN 9
+
+MFRC522 mfrc522(SS_PIN, RST_PIN); // Create MFRC522 instance.
+
+byte card_ID[4]; //card UID size 4byte
+byte Name1[4]={0x13,0x75,0x08,0x35};//first UID card
+byte Name2[4]={0xD3,0x80,0x68,0xFA};//second UID card
+
+//if you want the arduino to detect the cards only once
+int NumbCard[2];//this array content the number of cards. in my case i have just two cards.
+int j=0;        
+
+int const RedLed=6;
+int const GreenLed=5;
+int const Buzzer=8;
+
+String Name;//user name
+long Number;//user number
+int n ;//The number of card you want to detect (optional)  
+
+void setup() {
+  Serial.begin(9600); // Initialize serial communications with the PC
+  SPI.begin();  // Init SPI bus
+  mfrc522.PCD_Init(); // Init MFRC522 card
+  
+  Serial.println("CLEARSHEET");                 // clears starting at row 1
+  Serial.println("LABEL,Date,Time,Name,Number");// make four columns (Date,Time,[Name:"user name"]line 48 & 52,[Number:"user number"]line 49 & 53)
+
+
+
+   }
+    
+void loop() {
+  //look for new card
+   if ( ! mfrc522.PICC_IsNewCardPresent()) {
+  return;//got to start of loop if there is no card present
+ }
+ // Select one of the cards
+ if ( ! mfrc522.PICC_ReadCardSerial()) {
+  return;//if read card serial(0) returns 1, the uid struct contians the ID of the read card.
+ }
+ 
+ for (byte i = 0; i < mfrc522.uid.size; i++) {
+     card_ID[i]=mfrc522.uid.uidByte[i];
+
+       if(card_ID[i]==Name1[i]){
+       Name="Shaurya";//user name
+       Number=12;//user number
+       j=0;//first number in the NumbCard array
+      }
+      else if(card_ID[i]==Name2[i]){
+       Name="Aayush";//user name
+       Number=11;//user number
+       j=1;//Second number in the NumbCard array 
+      }
+      else{
+        Serial.println("SCAN");
+     }
+}
+      if(NumbCard[j] == 1){//to check if the carddetect already 
+      //if you want to use LCD
+      //Serial.println("Already Exist");
+      }
+      else{
+      NumbCard[j] = 1;//NumbCard[j]={1,1} to let the arduino know if the card was detecting 
+      n++;//(optional)
+      Serial.print("DATA,DATE,TIME," + Name);//send the Name to excel
+      Serial.print(",");
+      Serial.println(Number); //send the Number to excel
+
+      digitalWrite(Buzzer,HIGH);
+      delay(30);
+      digitalWrite(Buzzer,LOW);
+      Serial.println("SAVEWORKBOOKAS,Names/WorkNames");
+      }
+      delay(1000);
+
+
+//if you want to close the Excel when all card had detected and save Excel file (optional)
+//if(n==2){
+    
+  //  Serial.println("FORCEEXCELQUIT");
+ //   }
+}
+    
